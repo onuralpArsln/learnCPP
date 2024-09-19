c29f7bf8a4225a6554542b481aaafd1a35101cf0
hackerFaruk <onuralparslan97@gmail.com>
2024-09-19 11:07:16 +0300

    header updates

---
+++ Ders2/headerFiles/myHeader.h
@@ -0,0 +1,13 @@
+#ifndef MYFUNCTIONS_H // Guard Yapısı
+#define MYFUNCTIONS_H
+#include <string> // bunu yoruma almayı dene
+
+// Function declarations
+int add(int a, int b);
+void printMessage(const std::string& message);
+
+#endif 
+
+// Guard yapısının amacı -> Eğer cpp dosyanda bu headerı iki defa aktarmaya çalışırsan 
+// (unuttun ve iki kere importladın diyelim)
+// Bu yapı headrın iki kere tekrarlanıp gereksiz kaynak harcanmasına engel olur
